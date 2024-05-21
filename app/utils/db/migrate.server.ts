2fd8c85cd2b94437ab6c76028c4331008ec7d63d
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-05-21 10:25:51 -0300

    refactor: chalk in database seed

--- app/utils/db/migrate.server.ts
+++ app/utils/db/migrate.server.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk"
 import { env } from "../env"
 import { drizzle } from "drizzle-orm/postgres-js"
 import { migrate } from "drizzle-orm/postgres-js/migrator"
@@ -6,17 +7,17 @@ import postgres from "postgres"
 async function main() {
   const db = drizzle(postgres(env.DATABASE_URL, { max: 1 }))
 
-  console.log("Running migrations")
+  console.log(chalk.yellow("Running migrations"))
 
   await migrate(db, { migrationsFolder: "drizzle" })
 
-  console.log("Migrated successfully")
+  console.log(chalk.green("✔ Migrated successfully"))
 
   process.exit(0)
 }
 
 main().catch((e) => {
-  console.error("Migration failed")
+  console.error(chalk.red("Migration failed"))
   console.error(e)
   process.exit(1)
 })
--- app/utils/db/seed.server.ts
+++ app/utils/db/seed.server.ts
@@ -1,15 +1,34 @@
 import { getPasswordHash } from "../auth.server"
 import { db } from "./index.server"
 import { sessions, users } from "./schema"
+import chalk from "chalk"
 
-// Clear database
+/**
+ * Reset database
+ */
 await db.delete(sessions)
 await db.delete(users)
 
-await db.insert(users).values({
-  email: "admin@admin.com",
-  passwordHash: await getPasswordHash("admin"),
-  role: "admin",
-})
+console.log(chalk.yellow("✔ Database reseted"))
+
+/**
+ * Create users
+ */
+await db.insert(users).values([
+  {
+    email: "admin@admin.com",
+    passwordHash: await getPasswordHash("admin"),
+    role: "admin",
+  },
+  {
+    email: "user@user.com",
+    passwordHash: await getPasswordHash("user"),
+    role: "user",
+  },
+])
+
+console.log(chalk.yellow("✔ Users created"))
+
+console.log(chalk.green("✔ Database seeded"))
 
 process.exit(0)
